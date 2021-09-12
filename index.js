afa1a0934fabb7d3433151e99be592758d153560
Shahrizoda <87145658+Shahrizoda888@users.noreply.github.com>
2021-09-13 00:54:39 +0500

    change to index.html

--- index.js
+++
@@ -1,5 +0,0 @@
-const burgerIcon = document.querySelector('#burger');
-const navbarmenu = document.querySelector('#nav-links');
-burgerIcon.addEventListener('click', () => {
-    navbarmenu.classList.toggle('is-active');
-})
